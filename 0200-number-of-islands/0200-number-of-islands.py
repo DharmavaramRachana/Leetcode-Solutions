0629748a6a03fa389fcbe62cffb0a66bfef54586
DharmavaramRachana <118820308+DharmavaramRachana@users.noreply.github.com>
2026-02-25 14:24:10 -0500

    Time: 314 ms (5.18%), Space: 27.1 MB (15.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,33 +5,36 @@ class Solution:
 
 
         rows, cols = len(grid), len(grid[0])
+
         island = 0
-        visit = set()
+        visit= set()
+       
 
         def bfs(r,c):
             q = collections.deque()
-            q.append((r,c))
             visit.add((r,c))
+            q.append((r,c))
 
             while q:
                 row, col = q.popleft()
-                directions = [[0,1], [1, 0], [-1, 0], [0, -1]]
+
+                directions = [[1, 0], [-1, 0], [0, 1], [0,-1]]
 
                 for dr, dc in directions:
-                    r, c = row + dr , col + dc
-                    if (r in range(rows) and c in range(cols) and grid[r][c]== "1"    
-                         and (r, c) not in visit):
+                    r,c = row+dr, col+dc
 
+                    if (r in range(rows) and c in range(cols) and (r,c) not in visit and grid[r][c] == "1"):
                         q.append((r,c))
                         visit.add((r,c))
 
+
         for r in range(rows):
             for c in range(cols):
-                if (grid[r][c] == "1" and (r, c) not in visit):
+                if (grid[r][c] == "1" and (r,c) not in visit):
                     bfs(r,c)
                     island += 1
 
+
         return island
-
-
+                
         
