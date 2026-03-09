d23bb0a610872fa6ac7c2781c09ce2e468128a5e
DharmavaramRachana <118820308+DharmavaramRachana@users.noreply.github.com>
2026-03-09 10:18:20 -0400

    Time: 178 ms (84.85%), Space: 23.5 MB (68.18%) - LeetHub

---
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        MOD = 10**9 + 7
+        
+        dp0 = [[0] * (one + 1) for _ in range(zero + 1)]
+        dp1 = [[0] * (one + 1) for _ in range(zero + 1)]
+
+        # Base cases:
+        # only zeros
+        for i in range(1, min(zero, limit) + 1):
+            dp0[i][0] = 1
+
+        # only ones
+        for j in range(1, min(one, limit) + 1):
+            dp1[0][j] = 1
+
+        for i in range(zero + 1):
+            for j in range(one + 1):
+                if i == 0 and j == 0:
+                    continue
+
+                # Compute dp0[i][j]
+                if i > 0 and j > 0:
+                    dp0[i][j] = dp0[i - 1][j] + dp1[i - 1][j]
+                    if i - limit - 1 >= 0:
+                        dp0[i][j] -= dp1[i - limit - 1][j]
+                    dp0[i][j] %= MOD
+
+                    # Compute dp1[i][j]
+                    dp1[i][j] = dp0[i][j - 1] + dp1[i][j - 1]
+                    if j - limit - 1 >= 0:
+                        dp1[i][j] -= dp0[i][j - limit - 1]
+                    dp1[i][j] %= MOD
+
+        return (dp0[zero][one] + dp1[zero][one]) % MOD
