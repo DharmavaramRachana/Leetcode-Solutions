33a802ec589c6ce324425d74d3fd88d05274bbe2
DharmavaramRachana <118820308+DharmavaramRachana@users.noreply.github.com>
2026-03-10 13:54:03 -0400

    Time: 0 ms (100%), Space: 19.3 MB (60.92%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,27 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        #inorder to form a range we need consecutive numbers
+
+        output = []
+        if not nums:
+            return output
+
+        start = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1] + 1:
+                if start == nums[i-1]:
+                    output.append(str(start))
+                else:
+                    output.append(str(start) + "->" + str(nums[i-1]))
+
+                start = nums[i]
+
+        if start == nums[-1]:
+            output.append(str(start))
+
+        else:
+            output.append(str(start) + "->" + str(nums[-1]))
+
+        return output
+
+        
