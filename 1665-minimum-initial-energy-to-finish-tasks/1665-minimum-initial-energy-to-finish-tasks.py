79777e4df3618b01e3b8e568add8bd37371994fa
DharmavaramRachana <118820308+DharmavaramRachana@users.noreply.github.com>
2026-05-12 14:29:28 -0400

    Time: 59 ms (43.56%), Space: 55.5 MB (94.06%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        tasks.sort(key=lambda x: x[1] - x[0], reverse=True)
+
+        energy = 0
+        initial = 0
+
+        for actual, minimum in tasks:
+            if energy < minimum:
+                need = minimum - energy
+                initial += need
+                energy += need
+
+            energy -= actual
+
+        return initial
