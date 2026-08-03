3f12f1bbe43286e13ab842533652ff06e2786e9f
DharmavaramRachana <118820308+DharmavaramRachana@users.noreply.github.com>
2026-08-02 23:11:07 -0400

    Time: 0 ms (100%), Space: 19.2 MB (87.28%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,14 +1,14 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
         n = len(chars)
+
         write = 0
         i = 0
 
         while i < n:
             current_char = chars[i]
             count = 0
-
-            while i < n and chars[i] == current_char:
+            while i < n and current_char == chars[i]:
                 i += 1
                 count += 1
 
@@ -17,9 +17,9 @@ class Solution:
 
             if count > 1:
                 count_str = str(count)
+
                 for digit in count_str:
                     chars[write] = digit
                     write += 1
 
         return write
-        
