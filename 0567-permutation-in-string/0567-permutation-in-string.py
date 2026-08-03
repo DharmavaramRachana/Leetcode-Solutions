9a6c2a05535c74a70017a683eca58d48cc831348
DharmavaramRachana <118820308+DharmavaramRachana@users.noreply.github.com>
2026-08-02 22:53:30 -0400

    Time: 11 ms (96.52%), Space: 19.3 MB (85.99%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,16 +3,15 @@ class Solution:
         if len(s1) > len(s2):
             return False
 
-        s1_count = [0] * 26
-        window = [0] * 26
+        count, window = [0] * 26, [0] * 26
 
-        for ch in s1:
-            s1_count[ord(ch) - ord("a")] += 1
+        for char in s1:
+            count[ord(char) - ord("a")] += 1
 
         for i in range(len(s1)):
             window[ord(s2[i]) - ord("a")] += 1
 
-        if s1_count == window:
+        if count == window:
             return True
 
         for i in range(len(s1), len(s2)):
@@ -20,8 +19,7 @@ class Solution:
 
             window[ord(s2[i - len(s1)]) - ord("a")] -= 1
 
-            if s1_count == window:
+            if count == window:
                 return True
 
         return False
-        
